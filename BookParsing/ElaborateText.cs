45dc8a791a56119dfb2a5903c17b83a7f28ba801
Matteo Manzo <imatteo.manzo@gmail.com>
2015-03-27 21:10:21 +0000

    first test passes, stripping punctuation and numbers

---
+++ BookParsing/ElaborateText.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BookParsing
+{
+	public class ElaborateText
+	{
+		public string StripText (string input)
+		{
+			string[] CharsToRemove = { ";", ",", ".", "'", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "\n", "\t", "\r" };
+			foreach(string character in CharsToRemove)
+			{
+				input = input.Replace(character, "");
+			}
+			return input;
+		}
+	}
+}
+
---
+++ BookParsing/ElaborateTextTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace BookParsing
+{
+	[TestFixture ()]
+	public class ElaborateTextTest
+	{
+		[Test ()]
+		public void ItShouldStripPunctuationAndNumbers ()
+		{
+			ElaborateText file = new ElaborateText ();
+			Assert.AreEqual ("I like  code", file.StripText("I, like 2 code."));
+		}
+	}
+}
+
