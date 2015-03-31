30dd9e1540f3fa950e477ef6c4c5f47f9cf4a316
Matteo Manzo <imatteo.manzo@gmail.com>
2015-03-31 18:10:00 +0100

    edited console message

--- BookParsing/Program.cs
+++ BookParsing/Program.cs
@@ -15,7 +15,7 @@ namespace BookParsing
 
 		public string ReadFromFile()
 		{
-			Console.WriteLine ("Type some text or a file root: ");
+			Console.WriteLine ("Type some text or a file (has to be inside the folder): ");
 			string input = Console.ReadLine ();
 			if (File.Exists(input)) input = File.ReadAllText (input);
 			return input;
