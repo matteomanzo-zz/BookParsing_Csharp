dad1adafa2554271c0929f92c354661e1943afc7
Matteo Manzo <imatteo.manzo@gmail.com>
2015-04-04 15:13:56 +0200

    console output tests

---
+++ BookParsing/ProgramTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace BookParsing
+{
+	[TestFixture ()]
+	public class ProgramTest
+	{		
+		Program program;
+
+		[SetUp()]
+		public void Init()
+		{
+			program = new Program();
+		}
+			
+		[Test()]
+		public void ItShouldPrintAnInitialMessageOnScreen()
+		{
+			StringWriter output = new StringWriter();
+			Console.SetOut(output);
+			program.ReadFromFile ();
+			Assert.AreEqual ("Type some text or a file name (it has to be inside the folder): \n", output.ToString ());
+		}
+			
+		[Test()]
+		public void ItShouldReturnTheRightOutputOnTheConsole()
+		{
+			StringWriter output = new StringWriter();
+			Console.SetOut(output);
+			program.PrintOnScreen (program.Elaborate ("hello, hello."));
+			Assert.AreEqual ("hello\t \t \t  times: 2\tprime: √\n", output.ToString ());
+		}
+	}
+}
+
